e4293fb4d2565ebc8a3163163d850658ef7d157a
Iamtong <l>
2020-07-07 20:51:56 +0800

    fix 补充英文语言包

--- resources/lang/en/admins.php
+++ resources/lang/en/admins.php
@@ -1,7 +1,23 @@
 <?php
 
 return [
-    'state'               => 'Status',
-    'enabled'             => 'enabled',
-    'disabled'            => 'disabled',
+    'state'                 => 'Status',
+    'enabled'               => 'enabled',
+    'disabled'              => 'disabled',
+    'pass_update_at'        => 'Password modification time',
+    'login_at'              => 'Last login time',
+
+    'log_login'             => 'Login log',
+    'log_pass'              => 'Password modification log',
+    'input'                 => 'Input data',
+
+    'old_password'          => 'Original password',
+    'old_password_help'     => 'If you need to change your password, please fill in the original password',
+    'new_password'          => 'New password',
+    'old_password_empty'    => 'Please fill in the original password',
+    'old_password_wrong'    => 'The original password is wrong',
+    'password_expired'      => 'Your password will expire at :num . Please change your password before it expires.',
+
+    'remark'                => 'Remark',
+    'sys_reset_password'    => 'Administrator： :user Reset password',
 ];
